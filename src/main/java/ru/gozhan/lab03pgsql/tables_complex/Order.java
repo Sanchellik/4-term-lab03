119c7a20b8930d77feea77587477116b8dc8a69b
Sanchellik <sashagozhan@gmail.com>
2023-04-05 21:38:53 +0300

    Order.java changed

--- src/main/java/ru/gozhan/lab03pgsql/tables_complex/Order.java
+++ src/main/java/ru/gozhan/lab03pgsql/tables_complex/Order.java
@@ -1,15 +1,26 @@
 package ru.gozhan.lab03pgsql.tables_complex;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-//@AllArgsConstructor
+@AllArgsConstructor
 public class Order {
 
-//    private
+    private int id;
 
-    private final String path = "src/main/resources/orders.json";
+    private int clientId;
+
+    private int sessionId;
+
+    private int seat;
+
+    public Order(int clientId, int sessionId, int seat) {
+        this.clientId = clientId;
+        this.sessionId = sessionId;
+        this.seat = seat;
+    }
 
 }
